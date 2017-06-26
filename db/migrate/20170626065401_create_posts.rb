b81943d884ab86033eddfe71da09e7d11eaf0981
Markvin <maa_ku13@yahoo.ca>
2017-06-26 15:01:14 +0800

    Added fields to Post

--- db/migrate/20170626065401_create_posts.rb
+++ db/migrate/20170626065401_create_posts.rb
@@ -2,6 +2,8 @@ class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :name
+      t.string :title
+      t.text :body
 
       t.timestamps
     end
