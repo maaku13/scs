1b6b55a02dee82f550caba26e10a80a4783a3530
Markvin <maa_ku13@yahoo.ca>
2017-06-26 20:30:34 +0800

    added after_commit callback function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    after_commit do |post|
+        # Get the total number of post with same name
+        nameTotalPosts = Post.where("name='"+post.name+"'").count()
+        # Get blog record with entered name
+        blogRecord = Blog.find_by(name: post.name)
+        
+        # If nill, create one record
+        if blogRecord == nil
+            Blog.create(name: post.name, counter: nameTotalPosts)
+        else
+            blogRecord.counter = nameTotalPosts
+            blogRecord.save
+        end
+    end
 end
